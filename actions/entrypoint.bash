3869953401455a40cd3361c1cfed66c46e2180cd
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-09-10 11:54:23 +0530

    key generation added

--- actions/entrypoint.bash
+++ actions/entrypoint.bash
@@ -9,19 +9,24 @@ apt-get install -y ssh
 cd $GITHUB_WORKSPACE
 
 ls -al
-
-touch secretFile
-
-echo $PRIVATE_KEY >> secretFile
-
-
+hostname=139.59.11.116
 mkdir $HOME/.ssh
 
 SSH_DIR="$HOME/.ssh"
 
-echo "$PRIVATE_KEY" | tr -d '\r' > "$SSH_DIR/id_ed25519"
-chmod 600 "$SSH_DIR/id_ed25519"
-eval "$(ssh-agent -s)"
-ssh-add "$SSH_DIR/id_ed25519"
+ssh-keygen -t rsa -b 4096 -C "GH-actions-ssh-dep    loy-key" -f "$HOME/.ssh/id_rsa" -N ""
+
+	cat > /etc/ssh/ssh_config <<EOL
+
+Host $hostname
+	HostName $hostname
+	ProxyJump jumphost
+	UserKnownHostsFile /etc/ssh/known_hosts
+	User $ssh_user
+EOL
+# echo "$PRIVATE_KEY" | tr -d '\r' > "$SSH_DIR/id_ed25519"
+# chmod 600 "$SSH_DIR/id_ed25519"
+# eval "$(ssh-agent -s)"
+# ssh-add "$SSH_DIR/id_ed25519"
 
 rsync -avzhp $GITHUB_WORKSPACE/example.txt root@139.59.11.116:/home/jayshamnani
