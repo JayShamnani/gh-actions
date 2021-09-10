8c5e8869a4c117f7dcf466f0d54b303e1578cd96
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-09-10 10:46:23 +0530

    rsync command added

--- actions/entrypoint.bash
+++ actions/entrypoint.bash
@@ -12,4 +12,13 @@ echo $PRIVATE_KEY >> secretFile
 
 apt-get install openssh-client -y
 
-ssh -i secretFile root@139.59.11.116
+apt-get install rsync -y
+
+SSH_DIR="$HOME/.ssh"
+
+echo "$PRIVATE_KEY" | tr -d '\r' > "$SSH_DIR/id_ed25519"
+chmod 600 "$SSH_DIR/id_ed25519"
+eval "$(ssh-agent -s)"
+ssh-add "$SSH_DIR/id_ed25519"
+
+rsync -avzhp $GITHUB_WORKSPACE/example.txt root@139.59.11.116:/home/jayshamnani
