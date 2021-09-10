e512bc371230d17aba393f37f93a929cc34d7f81
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-09-10 14:06:36 +0530

    removed unwanted code

--- actions/entrypoint.bash
+++ actions/entrypoint.bash
@@ -19,7 +19,7 @@ SSH_DIR="$HOME/.ssh"
 
 echo "$PRIVATE_KEY" >> "$SSH_DIR/id_ed25519"
 chmod 600 "$SSH_DIR/id_ed25519"
-eval "$(ssh-agent -s)"
-ssh-add "$SSH_DIR/id_ed25519"
+# eval "$(ssh-agent -s)"
+# ssh-add "$SSH_DIR/id_ed25519"
 
 rsync -av $GITHUB_WORKSPACE/example.txt root@139.59.11.116:/tmp/
