984c8992b356011dabd05a26075e197212b6f655
shamnani.jay@gmail.com <shamnani.jay@gmail.com>
2021-09-10 12:08:54 +0530

    function added

--- actions/entrypoint.bash
+++ actions/entrypoint.bash
@@ -17,14 +17,36 @@ SSH_DIR="$HOME/.ssh"
 
 ssh-keygen -t rsa -b 4096 -C "GH-actions-ssh-dep    loy-key" -f "$HOME/.ssh/id_rsa" -N ""
 
+function configure_ssh_config() {
+
+if [[ -z "$JUMPHOST_SERVER" ]]; then
+	# Create ssh config file. `~/.ssh/config` does not work.
 	cat > /etc/ssh/ssh_config <<EOL
+Host $hostname
+HostName $hostname
+IdentityFile ${SSH_DIR}/signed-cert.pub
+IdentityFile ${SSH_DIR}/id_rsa
+User $ssh_user
+EOL
+else
+	# Create ssh config file. `~/.ssh/config` does not work.
+	cat > /etc/ssh/ssh_config <<EOL
+Host jumphost
+	HostName $JUMPHOST_SERVER
+	UserKnownHostsFile /etc/ssh/known_hosts
+	User $ssh_user
 
 Host $hostname
 	HostName $hostname
-	ProxyJump $hostname
+	ProxyJump jumphost
 	UserKnownHostsFile /etc/ssh/known_hosts
 	User $ssh_user
 EOL
+fi
+
+}
+
+configure_ssh_config
 # echo "$PRIVATE_KEY" | tr -d '\r' > "$SSH_DIR/id_ed25519"
 # chmod 600 "$SSH_DIR/id_ed25519"
 # eval "$(ssh-agent -s)"
